a0111dd9d2ee382544ea719e6498cd786948519e
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2022-05-06 14:49:31 +0500

    Sign In

--- Task 1/SceneDelegate.swift
+++ Task 1/SceneDelegate.swift
@@ -16,7 +16,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        callSignInViewController()
         guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    func callSignInViewController(){
+        let vc = SignInViewController(nibName: "SignInViewController", bundle: nil)
+        if self.window == nil {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+        }
+        self.window?.rootViewController = vc
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ Task 1/controller/SignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignInViewController.swift
+//  Task 1
+//
+//  Created by Bekhruz Hakmirzaev on 06/05/22.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
